83692c8388d310a899cac3141569112795007b36
salma <muabakosalma@gmail.com>
2024-07-01 11:05:51 +0100

    first push

---
+++ arithmetic.py
@@ -0,0 +1,13 @@
+def addition_of_numbers(num1, num2):
+   return(num1+num2)
+
+
+def subtraction_of_numbers(num1, num2):
+    return (num1-num2)
+
+
+def multiplication(num1, num2):
+    return num1 * num2
+
+def division(num1, num2):
+    return num1/num2
---
+++ main.py
@@ -0,0 +1,5 @@
+import arithmetic
+num1 = 8
+num2 = 4
+print(arithmetic.subtraction_of_numbers(num1, num2))
+print(arithmetic.division(num1, num2))
